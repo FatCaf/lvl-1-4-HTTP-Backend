dafca35a14bed659cecb87b863fd375475abf3a9
Mykyta Neklesa <nikitaneklesa5@gmail.com>
2023-11-20 18:04:06 +0200

    .

--- script.js
+++ script.js
@@ -1,41 +1,19 @@
+import { processData } from "./dataProcessor.js";
 import { renderData, renderHeaders, renderStructure } from "./dataRenderer.js";
+import { getUrl } from "./tableConfig.js";
 
 function DataTable() {
+  // let userObject;
+  // let url = getUrl();
+  // try {
+  //   userObject = processData("GET", url);
+  // } catch (error) {
+  //   console.log(error.message);
+  // }
+
   renderStructure();
   renderHeaders();
   renderData();
 }
 
-function attachDeleteButtonListeners() {
-  const deleteButtons = document.querySelectorAll(".delete__button");
-
-  deleteButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-      let toDelete = `https://mock-api.shpp.me/mneklesa/users/${button.getAttribute(
-        "data-id"
-      )}`;
-
-      deleteUser("DELETE", toDelete)
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-          }
-          console.log("Delete successful!");
-        })
-        .catch((error) => {
-          console.error("Error: ", error.message);
-        });
-    });
-  });
-}
-
 DataTable();
-
-function deleteUser(method, url) {
-  return fetch(url, {
-    method: method,
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-}
