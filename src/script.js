64eace538b5e6d6fc53e1e5938b4774a7e94efc0
Mykyta Neklesa <nikitaneklesa5@gmail.com>
2023-11-28 17:36:40 +0200

    moved all imports here

--- src/script.js
+++ src/script.js
@@ -6,11 +6,22 @@ import {
   renderStructure,
 } from "./tableRender/dataRenderer";
 
+import { getColumns, getUrl, getParent } from "./tableRender/tableConfig";
+
+import processData from "./tableRender/dataProcessor";
+
 import addUserHandler from "./tableActions/addUser";
 import findUserHandler from "./tableActions/findUser";
+import deleteUserHandler from "./tableActions/deleteUser";
 
-renderStructure();
-renderHeaders();
-renderData();
+const columns = getColumns();
+const url = getUrl();
+const parent = getParent();
+
+const userObject = await processData("GET", url);
+
+renderStructure(parent);
+renderHeaders(columns);
+if (renderData(userObject)) deleteUserHandler(url);
 addUserHandler();
 findUserHandler();
