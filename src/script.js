b2993644a90dd7cd829c2c9be4e4265df12eb4e4
Mykyta Neklesa <nikitaneklesa5@gmail.com>
2023-11-30 14:09:46 +0200

    now user may render more than one config

--- src/script.js
+++ src/script.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-restricted-syntax */
 import "./styles/style.css";
 
 import {
@@ -6,7 +7,12 @@ import {
   renderStructure,
 } from "./tableRender/dataRenderer";
 
-import { getColumns, getUrl, getParent } from "./tableRender/tableConfig";
+import {
+  getColumns,
+  getUrl,
+  getParent,
+  configs,
+} from "./tableRender/tableConfig";
 
 import processData from "./tableRender/dataProcessor";
 
@@ -14,14 +20,30 @@ import addUserHandler from "./tableActions/addUser";
 import findUserHandler from "./tableActions/findUser";
 import deleteUserHandler from "./tableActions/deleteUser";
 
-const columns = getColumns();
-const url = getUrl();
-const parent = getParent();
+function delay(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
 
-const userObject = await processData("GET", url);
+async function dataTable(config) {
+  for (const element of config) {
+    const columns = getColumns(element);
+    const url = getUrl(element);
+    const parent = getParent(element);
 
-renderStructure(parent);
-renderHeaders(columns);
-if (renderData(userObject)) deleteUserHandler(url);
-addUserHandler();
-findUserHandler();
+    // eslint-disable-next-line no-await-in-loop
+    const userObject = await processData("GET", url);
+
+    renderStructure(parent);
+    renderHeaders(columns);
+    if (renderData(userObject, columns)) deleteUserHandler(url);
+    addUserHandler();
+    findUserHandler();
+
+    // eslint-disable-next-line no-await-in-loop
+    await delay(1000);
+  }
+}
+
+await dataTable(configs);
