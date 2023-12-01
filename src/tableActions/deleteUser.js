c576ff99910d51675bcbabb50bbd4fc9a8685d56
Mykyta Neklesa <nikitaneklesa5@gmail.com>
2023-12-01 17:34:58 +0200

    add a hash for each delete btn

--- src/tableActions/deleteUser.js
+++ src/tableActions/deleteUser.js
@@ -2,13 +2,13 @@
 import { renderData } from "../tableRender/dataRenderer";
 import processData from "../tableRender/dataProcessor";
 
-export default function deleteUserHandler(url) {
-  const deleteButtons = document.querySelectorAll(".delete__button");
+export default function deleteUserHandler(url, columns, hash) {
+  const deleteButtons = document.querySelectorAll(`.${hash}__delete__button`);
 
   [...deleteButtons].forEach((button) => {
     button.addEventListener("click", () => {
       const toDelete = `${url}/${button.getAttribute("data-id")}`;
-      deleteUser(toDelete, url);
+      deleteUser(toDelete, url, columns, hash);
     });
   });
 }
@@ -18,7 +18,7 @@ export default function deleteUserHandler(url) {
  *
  * @param {string} url User url which need to be deleted.
  */
-async function deleteUser(toDelete, toRefresh) {
+async function deleteUser(toDelete, toRefresh, columns, hash) {
   await fetch(toDelete, {
     method: "DELETE",
     headers: {
@@ -29,8 +29,8 @@ async function deleteUser(toDelete, toRefresh) {
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
-      const userObject = await processData("GET", toRefresh);
-      renderData(userObject);
+      const dataObject = await processData("GET", toRefresh);
+      renderData(dataObject, columns, hash);
     })
     .catch((error) => {
       throw new Error(error.message);
